35753c5ebe2dacd896586d6f1283b42db483f1a5
Alexey Nikitin <nikitin@bookmate.com>
2018-12-06 15:12:59 +0300

    fix currency rate

--- lib/kirpich/providers/currency.rb
+++ lib/kirpich/providers/currency.rb
@@ -1,33 +1,38 @@
 module Kirpich::Providers
   class Currency
     class << self
-      def currency_url(currency_id)
-        "http://world.investfunds.ru/ajax/graph.currency.php?q=#{currency_id}"
-      end
 
       def usd_rub
-        url = currency_url(493)
-        currency = ''
-        response = Faraday.get(url)
-        parsed_json = JSON.parse(response.body)
-        currency = parsed_json[0]['data'].last[1] if parsed_json[0]['data']
-        { name: 'USD', rate: currency, emoji: '💵' }
+        url = 'https://quote.rbc.ru/data/simple/delay/ticker/selt.0/59109'
+        { name: 'USD', rate: rate(url), emoji: '💵' }
+      end
+
+      def btc_usd
+        url = 'https://quote.rbc.ru/data/simple/delay/ticker/crypto.0/157694'
+        { name: 'BTC', rate: rate(url), emoji: '₿' }
       end
 
       def eur_usd
-        url = currency_url(495)
-        currency = ''
-        response = Faraday.get(url)
-        parsed_json = JSON.parse(response.body)
-        currency = parsed_json[0]['data'].last[1] if parsed_json[0]['data']
-        { name: 'EUR', rate: currency, emoji: '💶' }
+        url = 'https://quote.rbc.ru/data/simple/delay/ticker/selt.0/59089'
+        currency = rate(url)
+        { name: 'EUR', rate: rate(url), emoji: '💶' }
       end
 
       def usd_rub_eur_rub_btc
         rates = []
         rates << usd_rub
         rates << eur_usd
-        rates
+        rates.compact
+      end
+
+      private
+
+      def rate(url)
+        response = Faraday.get(url)
+        if response.success?
+          data = JSON.parse(response.body).dig('result', 'data')
+          data[0][7] if data
+        end
       end
     end
   end
